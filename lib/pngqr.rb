7aac582983af4d002b9434cd83115604180237f6
Andrew Snow <andrew@modulus.org>
2011-03-17 17:05:22 +1100

    v0.4. Auto-sizing output, border support

--- lib/pngqr.rb
+++ lib/pngqr.rb
@@ -6,20 +6,38 @@ class Pngqr
   class << self
 
     def encode(*opts)
+      opthash = {}
       if Hash===opts.last
-        @filename = opts.last.delete(:file) 
-        @scale = opts.last.delete(:scale)
+        opthash = opts.pop
+        @filename = opthash.delete(:file) 
+        @scale = opthash.delete(:scale)
+        @border = opthash.delete(:border)
       end
       @scale ||= 1
+      @border ||= 0
 
-      qr = RQRCode::QRCode.new(*opts)
+      qr = nil
+      if(opthash[:size]) # user supplied size
+        qr = RQRCode::QRCode.new(*opts, opthash)
+      else
+        # autosize algorithm: start at size=1 and increment until it worked
+        opthash[:size] = 1
+        while qr.nil?
+          qr = begin
+            RQRCode::QRCode.new(*opts, opthash)
+          rescue RQRCode::QRCodeRunTimeError => e
+            opthash[:size] += 1
+            raise unless e.to_s =~ /overflow/ && opthash[:size] <= 40
+          end
+        end
+      end
       len = qr.module_count
-      png = ChunkyPNG::Image.new(len*@scale, len*@scale, ChunkyPNG::Color::WHITE)
+      png = ChunkyPNG::Image.new(len*@scale + 2*@border, len*@scale + 2*@border, ChunkyPNG::Color::WHITE)
       
       for_each_pixel(len) do |x,y|
         if qr.modules[y][x]
           for_each_scale(x, y, @scale) do |scaled_x, scaled_y|
-            png[scaled_x, scaled_y] = ChunkyPNG::Color::BLACK
+            png[scaled_x + @border, scaled_y + @border] = ChunkyPNG::Color::BLACK
           end
         end
       end
